c82842038be5a066c56d849030347c21b711f57a
ryanlaw <ryan_law-rebound@hotmail.com>
2025-05-06 12:07:34 +0800

    enable location services prompt for dummy data

--- components/features/restaurants/NearbyRestaurants.tsx
+++ components/features/restaurants/NearbyRestaurants.tsx
@@ -678,7 +678,25 @@ export const NearbyRestaurants = ({
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                             </svg>
-                            You are viewing sample restaurant data. Enable location services to see restaurants near you.
+                            <span className="flex-1">
+                                You are viewing sample restaurant data.{' '}
+                                <button
+                                    onClick={() => {
+                                        if (navigator.geolocation) {
+                                            navigator.geolocation.getCurrentPosition(
+                                                () => window.location.reload(),
+                                                (error) => alert('Please enable location services in your browser settings.')
+                                            );
+                                        } else {
+                                            alert('Geolocation is not supported by this browser.');
+                                        }
+                                    }}
+                                    className="text-blue-600 hover:text-blue-800 underline font-medium focus:outline-none"
+                                >
+                                    Click here
+                                </button>
+                                {' '}to enable location services and see restaurants near you.
+                            </span>
                         </p>
                     </div>
                 )}
